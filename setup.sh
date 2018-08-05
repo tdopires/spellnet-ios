c7491f5b94965c92d9cde88efbc5973aed110b83
Túlio Bazan da Silva <tucobz@gmail.com>
2018-08-05 11:21:10 -0300

    Add initial project and Carthage

---
+++ setup.sh
@@ -0,0 +1,5 @@
+gem install bundler
+$SUDO_USER bundle install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install carthage
+carthage update --platform iOS
